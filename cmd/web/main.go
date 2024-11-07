fe78c6d10a734f6c00f5ed2ace6e0560c1536048
faizinahsan <faizin110998@gmail.com>
2024-11-07 21:34:52 +0700

    fix: structure folder

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,15 @@ import (
 )
 
 func main() {
+	viperConfig := config.NewViper()
 	app := config.NewGin()
-	config.Bootstrap(&config.BootstrapConfig{App: app})
-	err := app.Run(fmt.Sprintf("localhost:8080"))
+	config.Bootstrap(&config.BootstrapConfig{
+		App:    app,
+		Config: viperConfig,
+	})
+	host := viperConfig.GetString("web.host")
+	port := viperConfig.GetString("web.port")
+	err := app.Run(fmt.Sprintf("%s:%s", host, port))
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
--- internal/config/app.go
+++ internal/config/app.go
@@ -2,12 +2,14 @@ package config
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"personal-projects/templateProjects/internal/delivery/http"
 	"personal-projects/templateProjects/internal/delivery/http/route"
 )
 
 type BootstrapConfig struct {
-	App *gin.Engine
+	App    *gin.Engine
+	Config *viper.Viper
 }
 
 func Bootstrap(config *BootstrapConfig) {
